4496be1d52ac0f75ada3f8eec509120a345258ca
Akshay Kumar Mandal <akshaycodex@email.com>
2025-09-10 10:00:13 +0530

    fixed

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -2,7 +2,7 @@ import {Request ,Response , NextFunction} from 'express';
 import Jwt from 'jsonwebtoken';
 
 interface AuthRequest extends Request{
-    userId?:string
+    userId?:any;
 }
 
 const authMiddleware = ( req: AuthRequest,res: Response,next: NextFunction)=>{
